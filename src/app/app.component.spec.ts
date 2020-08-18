e28433ac22391ee6287fde9c7a56543820b343cc
Vaibhav Bhandare <vaibhav_bhandare@persistent.co.in>
2020-08-18 19:52:14 +0530

    erf

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'A10First'`, () => {
+  it(`should have as title 'A10First' `, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('A10First');
